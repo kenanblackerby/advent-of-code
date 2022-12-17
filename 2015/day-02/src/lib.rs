79e24db256dbfdfebc4aef6f6c2e11855149363e
Kenan Blackerby <kblackerby2@gmail.com>
2022-12-17 13:20:36 -0600

    add part 2 day 02, 2015

--- 2015/day-02/src/lib.rs
+++ 2015/day-02/src/lib.rs
@@ -13,6 +13,21 @@ pub fn process_part1(input: &str) -> u32 {
         .sum()
 }
 
+pub fn process_part2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|dimensions| {
+            let dimensions = dimensions
+                .split('x')
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>();
+            let volume = dimensions[0] * dimensions[1] * dimensions[2];
+            let wrapping_ribbon = calculate_smallest_perimeter(dimensions[0], dimensions[1], dimensions[2]);
+            volume + wrapping_ribbon
+        })
+        .sum()
+}
+
 fn calculate_surface_area(length: u32, width: u32, height: u32) -> u32 {
     2 * length * width + 2 * length * height + 2 * width * height
 }
@@ -24,6 +39,16 @@ fn calculate_overage(length: u32, width: u32, height: u32) -> u32 {
         length * width
     } else {
         height * width
+    }
+}
+
+fn calculate_smallest_perimeter(length: u32, width: u32, height: u32) -> u32 {
+    if length < width && height < width {
+        2 * (length + height)
+    } else if length < height {
+        2 * (length + width)
+    } else {
+        2 * (height + width)
     }
 }
 
@@ -49,7 +74,13 @@ mod tests {
     fn should_process_part1() {
         assert_eq!(process_part1("2x3x4"), 58);
         assert_eq!(process_part1("1x1x10"), 43);
-
         assert_eq!(process_part1(INPUT), 101);
+    }
+
+    #[test]
+    fn should_process_part2() {
+        assert_eq!(process_part2("2x3x4"), 34);
+        assert_eq!(process_part2("1x1x10"), 14);
+        assert_eq!(process_part2(INPUT), 48);
     }
 }
